4551764a27382faa10d7c99417c14d53fbd5c206
kjymonster <67585407+kjymonster@users.noreply.github.com>
2020-08-24 14:49:00 +0900

    애니메이션 숫자 순서대로 구현 성공

--- app/src/main/java/com/example/foodsreadycallservice/MainActivity.kt
+++ app/src/main/java/com/example/foodsreadycallservice/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.foodsreadycallservice
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.view.View.INVISIBLE
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.animation.Animation.AnimationListener
+import android.widget.Switch
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class MainActivity : BaseActivity() {
+
+    private lateinit var numQueue : Queue<Int>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,64 +23,79 @@ class MainActivity : BaseActivity() {
 
         var anim: Animation
         var start = false
-
         var clickCount = 0
+        var i = 0
+
+//        var numList = arrayOfNulls<Int>(13)
 
 
         callBtn.setOnClickListener {
-
             popNumTxt.visibility = View.VISIBLE
-
             clickCount = clickCount + 1
-            popNumTxt.text = "${clickCount}"
 
-            val callNumList: MutableList<Int> = mutableListOf(clickCount)
-            callNumList.add(clickCount)
-            popNumTxt.text = "${callNumList[0]}"
+//            for (i in 0..12) {
+//                numList[i] = clickCount
+//                if (i > 12) {
+//                    i == 0
+//                    continue
+//                }
+//            }
 
-           Handler().postDelayed({
-                   popNumTxt.visibility = View.GONE
-               }, 3000)
+//            when(clickCount){
+//                1 -> numList[0] = clickCount
+//                2 -> numList[1] = clickCount
+//                3 -> numList[2] = clickCount
+//                4 -> numList[3] = clickCount
+//
+//            }
+
+            numQueue = LinkedList()
+
+            numQueue.offer(clickCount)
+
+           popNumTxt.text = "${numQueue.poll()}"
+
+            Handler().postDelayed({
+                popNumTxt.visibility = View.GONE
+            }, 3000)
+
+
 
             mainNumTxt.text = popNumTxt.text
 
-
             anim = AlphaAnimation(0.0f, 1.0f) //투명도. 0.0~1.0
-            anim.setDuration(1000)  //지속시간. (100) = 100 = 1초
-            //anim.setStartOffset(20) //한번 애니메이션이 시작하기위해 대기하는 시간
-            //anim.setRepeatMode(Animation.REVERSE) //반복
-            anim.setRepeatCount(4) //몇번 반복할지
+            anim.setDuration(1500)  //지속시간. (100) = 100 = 1초
+//            anim.setStartOffset(10) //한번 애니메이션이 시작하기위해 대기하는 시간
+//            anim.setRepeatMode(Animation.REVERSE) //반복
+            anim.setRepeatCount(5) //몇번 반복할지
 
-            if (mainNumTxt != null) {
+            if (mainNumTxt.text != null) {
                 if (!start) {
-                    start = true;
+//                    start = true
                     mainNumTxt.startAnimation(anim)
                     mainNumTxt.visibility = View.INVISIBLE
 
-                    anim.setAnimationListener(object : AnimationListener {
-                        override fun onAnimationRepeat(p0: Animation?) {}
 
-                        override fun onAnimationEnd(animation: Animation) {
-
-                            subNumTxt01.text = mainNumTxt.text
-                        }
-
-                        override fun onAnimationStart(p0: Animation?) {}
-                    })
                 } else {
                     start = false;
                     mainNumTxt.clearAnimation();
                 }
+                anim.setAnimationListener(object : AnimationListener {
+                    override fun onAnimationRepeat(p0: Animation?) {}
 
+                    override fun onAnimationEnd(animation: Animation) {
+                        subNumTxt01.text = mainNumTxt.text
+                        subNumTxt02.text = subNumTxt02.text
+                        subNumTxt03.text = "${numQueue.poll()}"
+                    }
 
-//            Handler().postDelayed(Runnable {
-//                //여기에 딜레이 후 시작할 작업들을 입력
-//                subNumTxt04.text = "${receivedNum}"
-//            }, 5500) // 5.5초 정도 딜레이를 준 후 시작
+                    override fun onAnimationStart(p0: Animation?) {
 
+                    }
+                })
             }
 
-          }
+        }
 
 
     }
